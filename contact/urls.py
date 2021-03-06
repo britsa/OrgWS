b941308ad618f85bf0725a6ef250750d4747bb60
Lavanya2899 <lavslyra28@gmail.com>
2021-03-06 14:31:40 +0530

    contact form has corrected in the url file

--- contact/urls.py
+++ contact/urls.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from contact.views import ContactForm
 
 urlpatterns = [
-    re_path(r'^$',ContactForm , name=''),
+    re_path(r'^$',ContactForm.as_view(), name=''),
 ]
