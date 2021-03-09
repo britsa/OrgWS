5bcf57370017bb80f83c999f017d173de3e65741
LAPTOP-VK7R9DDT <kaviyasobanbabu@gmail.com>
2021-03-08 21:11:27 -0800

    client name has been changed in url

--- client/urls.py
+++ client/urls.py
@@ -1,7 +1,7 @@
 from django.urls import re_path
 
-from client.views import client
+from client.views import Client
 
 urlpatterns = [
-    re_path(r'^$', client.as_view(), name = ''),
+    re_path(r'^$', Client.as_view(), name = ''),
 ]
