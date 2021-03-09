9a811e090b4a59cae45b6d7e194fe2207e77ceea
Regilan <mariairegilan@gmail.com>
2021-03-09 14:48:23 +0530

    exception handled code

--- access/views.py
+++ access/views.py
@@ -1,11 +1,23 @@
 from rest_framework import generics
 from rest_framework.response import Response
+from stash_service import HttpStatusCode
+
+from orgws_common.utils import requested_version
 
 
 class TokenHandler(generics.RetrieveAPIView, generics.CreateAPIView):
 
     def retrieve(self, request, *args, **kwargs):
-        pass
+        try:
+            version: str = requested_version(request, kwargs)
+            if version == '1':
+                pass
+            return Response({'message': request.headers}, status=HttpStatusCode.OK.status_code())
+        except:
+            return Response({'message': 'Failed'}, status=HttpStatusCode.INTERNAL_SERVER_ERROR.status_code())
 
     def create(self, request, *args, **kwargs):
-        pass
+        try:
+            pass
+        except:
+            pass
