0199141596c025278e44c602bff0d531a08496f8
LAPTOP-VK7R9DDT <kaviyasobanbabu@gmail.com>
2021-03-17 14:58:52 -0700

    started /client post

--- client/models.py
+++ client/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+class ContactInformation(object):
+    def __init__(self,body: dict):
+        self.__name: str = body[u'Name']
+        self.__mail_id: str = body[u'Mail-Id']
+        self.__phone: str = body[u'Phone']
+        self.__note: str = body[u'Note']
+        self.__primary: bool = body[u'Primary']
 
-# Create your models here.
+class ClientInformation(object):
+    def __init__(self, body: dict):
+        self.__admin_id: str = body[u'AdminId']
+        self.__company_name: str = body[u'CompanyName']
+        self.__contact: list =body[u'Contact']
+        
--- client/views.py
+++ client/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
 from rest_framework.response import Response
 
+from orgws_common.constants import HeaderKeys
+from orgws_common.utils import requested_version, error_response
+
+
 class Client(generics.RetrieveUpdateDestroyAPIView,generics.CreateAPIView,generics.ListAPIView):
 
     def retrieve(self, request, *args, **kwargs):
@@ -10,7 +14,14 @@ class Client(generics.RetrieveUpdateDestroyAPIView,generics.CreateAPIView,generi
         pass
 
     def create(self, request, *args, **kwargs):
-        pass
+        try:
+            version: str = requested_version(request, kwargs)
+            if version == '1':
+                token: str = request.headers[HeaderKeys.TOKEN]
+                body: dict = request.data
+                return Response (body)
+        except Exception as e:
+            return error_response(e)
 
     def partial_update(self, request, *args, **kwargs):
         pass
